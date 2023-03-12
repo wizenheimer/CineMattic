94d8e11f73dbded5d701e71031dd201ea743052e
kn4y4n <kn4y4nnn@gmail.com>
2023-03-13 00:59:08 +0530

    feat: CBV

--- Movies/urls.py
+++ Movies/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from .views import movie_list, movie_manip
+from .views import MovieListAV, MovieDetailAV
 
 urlpatterns = [
-    path("list/", movie_list, name="movie_list"),
-    path("list/<int:pk>/", movie_manip, name="movie_manip"),
+    path("list/", MovieListAV.as_view(), name="movie_list"),
+    path("list/<int:pk>/", MovieDetailAV.as_view(), name="movie_detail"),
 ]
--- Movies/views.py
+++ Movies/views.py
@@ -1,9 +1,6 @@
-from django.shortcuts import render
-
-# from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
@@ -11,14 +8,13 @@ from .models import Movie
 from .serializers import MovieSerializer
 
 
-@api_view(["GET", "POST"])
-def movie_list(request, format=None):
-    if request.method == "GET":
+class MovieListAV(APIView):
+    def get(self, request):
         movies = Movie.objects.all()
         serializer = MovieSerializer(movies, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    if request.method == "POST":
+    def post(self, request):
         serializer = MovieSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -27,14 +23,14 @@ def movie_list(request, format=None):
             return Response(serializer.errors)
 
 
-@api_view(["GET", "DELETE", "PUT"])
-def movie_manip(request, pk, format=None):
-    movie = get_object_or_404(Movie, pk=pk)
-    if request.method == "GET":
+class MovieDetailAV(APIView):
+    def get(self, request, pk):
+        movie = get_object_or_404(Movie, pk=pk)
         serializer = MovieSerializer(movie)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    if request.method == "PUT":
+    def put(self, request, pk):
+        movie = get_object_or_404(Movie, pk=pk)
         serializer = MovieSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -42,6 +38,7 @@ def movie_manip(request, pk, format=None):
         else:
             return Response(serializer.errors)
 
-    if request.method == "DELETE":
+    def delete(self, request, pk):
+        movie = get_object_or_404(Movie, pk=pk)
         movie.delete()
         return Response({"msg": "model deleted"}, status=status.HTTP_200_OK)
